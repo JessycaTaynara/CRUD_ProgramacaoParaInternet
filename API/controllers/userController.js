7f6f1f496e9a11c1140f17c0223682247bab301c
JessycaTaynara <jessycatay16@gmail.com>
2023-11-17 20:15:32 -0300

    .

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -6,11 +6,11 @@ class userController {
     try {
       const user = await userModel.login(email, senha);
       if (user.length == 0) {
-        return res.status(404).send({ message: "Email ou senha incorretos" });
+        return res.status(404).send({ mensagem: "Email ou senha incorretos" });
       }
       return res.status(200).send(user[0]);
     } catch (error) {
-      return res.status(404).send({ message: `Erro ao logar - ${error}` });
+      return res.status(404).send({ mensagem: `Erro ao logar - ${error}` });
     }
   }
   async show(req, res) {
@@ -20,27 +20,28 @@ class userController {
     } catch (error) {
       return res
         .status(500)
-        .send({ message: `Erro ao listar usuários - ${error}` });
+        .send({ mensagem: `Erro ao listar usuários - ${error}` });
     }
   }
   async create(req, res) {
     const { email, senha } = req.body;
     try {
-        const user = await userModel.createUser(email, senha);
-        return res.status(200).send(user); 
+      const user = await userModel.createUser(email, senha);
+      return res.status(200).send(user);
     } catch (error) {
-        return res.status(500).send({ message: `Erro ao criar usuário - ${error}` }); 
+      return res
+        .status(500)
+        .send({ mensagem: `Erro ao criar usuário - ${error}` });
     }
   }
   async remove(req, res) {
     const email = req.params.email;
     //const user = await userModel.find(email)
     //if (!user)
-      //return res.status(404).send({ message: "Usuário não encontrado" });
+    //return res.status(404).send({ mensagem: "Usuário não encontrado" });
     await userModel.remove(email);
-    res.status(200).send({ message: "Usuário deletado" });
+    res.status(200).send({ mensagem: "Usuário deletado" });
   }
-
 }
 
 export default new userController();
