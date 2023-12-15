4d88c383c5680a43fd83ebf3e912cc842ae97872
sabiss <sabrinabezsilva@gmail.com>
2023-12-15 08:10:49 -0300

    REFACTOR: convertendo para o camelCase e formatação com o prettier

--- src/models/gatoModel.js
+++ src/models/gatoModel.js
@@ -1,40 +1,47 @@
 import bancoDeDados from "../database/index.js";
 
 class gatoModel {
-  async adicionar_gato(nome, idade, sexo, raca, cor, descricao) {
+  async adicionarGato(nome, idade, sexo, raca, cor, descricao) {
     const conn = await bancoDeDados.conectar();
     const sql =
       "INSERT INTO gatos (nome, idade, sexo, raca, cor, descricao) VALUES ($1, $2, $3,$4, $5, $6)";
     return await conn.query(sql, [nome, idade, sexo, raca, cor, descricao]);
   }
-  async get_gatos(){
+  async getGatos() {
     const conn = await bancoDeDados.conectar();
-    const sql =
-      "SELECT * FROM gatos";
+    const sql = "SELECT * FROM gatos";
     const gatos = await conn.query(sql);
-    return gatos.rows
+    return gatos.rows;
   }
-  async get_gato_por_nome(nome){
+  async getGatoPorId(id) {
+    const conn = await bancoDeDados.conectar();
+    const sql = "SELECT * FROM gatos WHERE id = ($1)";
+    const gatos = await conn.query(sql, [id]);
+    return gatos.rows[0];
+  }
+  async deleteGato(nome) {
+    const conn = await bancoDeDados.conectar();
+    const sql = "DELETE FROM gatos WHERE nome = ($1)";
+    return await conn.query(sql, [nome]);
+  }
+  async deletarAdocao(nomeGato) {
+    const conn = await bancoDeDados.conectar();
+    const sql = "DELETE FROM adocoes WHERE gato = ($1)";
+    return await conn.query(sql, [nomeGato]);
+  }
+  async update(nomeAntigo, novoNome, idade, sexo, raca, cor, descricao) {
     const conn = await bancoDeDados.conectar();
     const sql =
-      "SELECT * FROM gatos WHERE nome = ($1)";
-    const gatos = await conn.query(sql, [nome]);
-    return gatos.rows[0]
-  }
-  async deleteGato(nome){
-    const conn = await bancoDeDados.conectar();
-    const sql = "DELETE FROM gatos WHERE nome = ($1)"
-    return await conn.query(sql, [nome])
-  }
-  async deletarAdocao(nomeGato){
-    const conn = await bancoDeDados.conectar();
-    const sql = "DELETE FROM adocoes WHERE gato = ($1)"
-    return await conn.query(sql, [nomeGato])
-  }
-  async update(nomeAntigo, novoNome, idade, sexo, raca, cor, descricao){
-    const conn = await bancoDeDados.conectar()
-    const sql = "UPDATE gatos SET nome = ($1), idade = ($2), sexo = ($3), raca = ($4), cor = ($5), descricao = ($6) WHERE nome = ($7)"
-    return await conn.query(sql, [novoNome, idade, sexo, raca, cor, descricao, nomeAntigo])
+      "UPDATE gatos SET nome = ($1), idade = ($2), sexo = ($3), raca = ($4), cor = ($5), descricao = ($6) WHERE nome = ($7)";
+    return await conn.query(sql, [
+      novoNome,
+      idade,
+      sexo,
+      raca,
+      cor,
+      descricao,
+      nomeAntigo,
+    ]);
   }
 }
 
