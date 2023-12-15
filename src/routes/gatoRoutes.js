be2f7944b7b3bac473ddaefe41590c26a362828f
sabiss <sabrinabezsilva@gmail.com>
2023-12-15 09:00:38 -0300

    REFACTOR: retirando método de getSeuGatosAdotados

--- src/routes/gatoRoutes.js
+++ src/routes/gatoRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import gatoController from "../controllers/gatoController.js";
 
-const routes = new Router()
+const routes = new Router();
 
 routes
-    .post('/addGato', gatoController.addGato)
-    .get('/gatos', gatoController.getGatos)
-    .get('/meusGatosAdotados/:email', gatoController.getSeuGatosAdotados)
-    .delete('/deletarGato', gatoController.excluirGato)
-    .delete('/deletarAdocao', gatoController.deletarAdocao)
-    .put('/atualizarGato', gatoController.editarGato)
-export default routes
+  .post("/addGato", gatoController.addGato)
+  .get("/gatos", gatoController.getGatos)
+
+  .delete("/deletarGato", gatoController.excluirGato)
+  .delete("/deletarAdocao", gatoController.deletarAdocao)
+
+  .put("/atualizarGato", gatoController.editarGato);
+export default routes;
