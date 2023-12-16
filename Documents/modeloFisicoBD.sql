650197f53b6eb12d29252a9429cd46a74bc3b64c
sabiss <sabrinabezsilva@gmail.com>
2023-12-16 13:01:09 -0300

    FIX: nome correto da tabela

--- Documents/modeloFisicoBD.sql
+++ Documents/modeloFisicoBD.sql
@@ -33,6 +33,6 @@ CREATE TABLE solicitacoes(
 CREATE TABLE adocoes(
 	id_gato Int NOT NULL,
 	email_usuario VARCHAR(255) NOT NULL,
-	FOREIGN KEY (email_usuario) REFERENCES usuarios_comuns(email),
+	FOREIGN KEY (email_usuario) REFERENCES usuarios(email),
 	FOREIGN KEY (id_gato) REFERENCES gatos(id)
 );
