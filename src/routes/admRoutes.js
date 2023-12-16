30020f67c7d217200f16ddbbc6cb6f65fd5e71c6
sabiss <sabrinabezsilva@gmail.com>
2023-12-16 16:24:19 -0300

    FEAT: rota para aceitar solicitações

--- src/routes/admRoutes.js
+++ src/routes/admRoutes.js
@@ -3,5 +3,9 @@ import admController from "../controllers/admController.js";
 
 const routes = new Router();
 
-routes.get("/solicitacoes", admController.getSolicitacoes);
+routes
+  .get("/solicitacoes", admController.getSolicitacoes)
+
+  .post("/aceitarSolicitação/:id", admController.aceitarSolicitacao);
+
 export default routes;
