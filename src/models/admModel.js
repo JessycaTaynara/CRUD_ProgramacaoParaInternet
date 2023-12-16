71c067a5989510904ea9029fddd6aa1f9d795617
sabiss <sabrinabezsilva@gmail.com>
2023-12-16 13:09:29 -0300

    FEAT: enviar gato da solicitação para que possa ser adotado

--- src/models/admModel.js
+++ src/models/admModel.js
@@ -7,6 +7,18 @@ class AdmModel {
     const solicitacoes = await conn.query(sql);
     return solicitacoes.rows;
   }
+  async colocarGatoDaSolicitacaoParaAdocao(solicitacao) {
+    const conn = await bancoDeDados.conectar();
+    const sql =
+      "INSERT INTO gatos (nome, sexo, cor, descricao, raca) VALUES ($1,$2,$3,$4,$5)";
+    await conn.query(sql, [
+      solicitacao.gato_nome,
+      solicitacao.gato_sexo,
+      solicitacao.gato_cor,
+      solicitacao.gato_descricao,
+      solicitacao.gato_raca,
+    ]);
+  }
 }
 
 export default new AdmModel();
