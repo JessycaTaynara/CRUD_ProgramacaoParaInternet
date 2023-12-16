92d7a03ef6b56c1a3c43f61d7784932c4f86136a
sabiss <sabrinabezsilva@gmail.com>
2023-12-16 13:13:58 -0300

    ADM pode negar uma solicitação

--- src/models/admModel.js
+++ src/models/admModel.js
@@ -8,6 +8,7 @@ class AdmModel {
     return solicitacoes.rows;
   }
   async colocarGatoDaSolicitacaoParaAdocao(solicitacao) {
+    //aceitar solicitação
     const conn = await bancoDeDados.conectar();
     const sql =
       "INSERT INTO gatos (nome, sexo, cor, descricao, raca) VALUES ($1,$2,$3,$4,$5)";
@@ -23,6 +24,12 @@ class AdmModel {
     const conn = await bancoDeDados.conectar();
     const sql = "DELETE FROM solicitacoes WHERE id = ($1)";
     await conn.query(sql, [id]);
+  }
+  async negarSolicitacao(id) {
+    const conn = await bancoDeDados.conectar();
+    const sql =
+      "UPDATE solicitacoes SET solicitacao_rejeitada = true WHERE id = ($1)";
+    conn.query(sql, [id]);
   }
 }
 
