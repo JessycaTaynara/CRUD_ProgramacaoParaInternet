c5bf41335f1681523369d0ec48c6593a21b88df0
sabiss <sabrinabezsilva@gmail.com>
2023-12-16 11:53:20 -0300

    FEAT: rota de solicitação

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import cors from "cors";
 import usuario from "./src/routes/userRoutes.js";
 import gato from "./src/routes/gatoRoutes.js";
 import login from "./src/routes/authRoutes.js";
+import administrador from "./src/routes/admRoutes.js";
 
 const app = express();
 
-app.use(cors("*"), express.json(), usuario, gato, login);
+app.use(cors("*"), express.json(), usuario, gato, login, administrador);
 
 app.listen(3000, () => {
   console.log("Api rodando");
