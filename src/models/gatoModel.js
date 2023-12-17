abcf25fc2327372f5fa085e631557a7834b5c7b7
sabiss <sabrinabezsilva@gmail.com>
2023-12-17 13:28:59 -0300

    REFACTOR:  novo nome da tabela de gatos, getGatos agora é getGatosParaAdotar

--- src/models/gatoModel.js
+++ src/models/gatoModel.js
@@ -4,35 +4,35 @@ class gatoModel {
   async adicionarGato(nome, sexo, raca, cor, descricao) {
     const conn = await bancoDeDados.conectar();
     const sql =
-      "INSERT INTO gatos (nome, sexo, raca, cor, descricao) VALUES ($1, $2, $3,$4, $5)";
+      "INSERT INTO gatos_para_adotar (nome, sexo, raca, cor, descricao) VALUES ($1, $2, $3,$4, $5)";
     return await conn.query(sql, [nome, sexo, raca, cor, descricao]);
   }
-  async getGatos() {
+  async getGatosParaAdocao() {
     const conn = await bancoDeDados.conectar();
-    const sql = "SELECT * FROM gatos";
+    const sql = "SELECT * FROM gatos_para_adotar WHERE adotado=false";
     const gatos = await conn.query(sql);
     return gatos.rows;
   }
   async getGatoPorId(id) {
     const conn = await bancoDeDados.conectar();
-    const sql = "SELECT * FROM gatos WHERE id = ($1)";
+    const sql = "SELECT * FROM gatos_para_adotar WHERE id = ($1)";
     const gatos = await conn.query(sql, [id]);
     return gatos.rows[0];
   }
-  async deleteGato(nome) {
+  async deleteGato(id) {
     const conn = await bancoDeDados.conectar();
-    const sql = "DELETE FROM gatos WHERE nome = ($1)";
-    return await conn.query(sql, [nome]);
+    const sql = "DELETE FROM gatos_para_adotar WHERE id = ($1)";
+    return await conn.query(sql, [id]);
   }
-  async deletarAdocao(nomeGato) {
+  async deletarAdocao(id) {
     const conn = await bancoDeDados.conectar();
-    const sql = "DELETE FROM adocoes WHERE gato = ($1)";
-    return await conn.query(sql, [nomeGato]);
+    const sql = "DELETE FROM adocoes WHERE id_gato = ($1)";
+    return await conn.query(sql, [id]);
   }
   async update(novoNome, novoSexo, novaRaca, novaCor, novaDescricao, id) {
     const conn = await bancoDeDados.conectar();
     const sql =
-      "UPDATE gatos SET nome = ($1), sexo = ($2), raca = ($3), cor = ($4), descricao = ($5) WHERE id = ($6)";
+      "UPDATE gatos_para_adotar SET nome = ($1), sexo = ($2), raca = ($3), cor = ($4), descricao = ($5) WHERE id = ($6)";
     return await conn.query(sql, [
       novoNome,
       novoSexo,
