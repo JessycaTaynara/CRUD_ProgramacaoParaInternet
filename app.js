1f1f1ad222286003236dfb95f498ba0fabc623d0
sabiss <sabrinabezsilva@gmail.com>
2023-12-17 13:52:29 -0300

    FEAT: presença do authMiddleware

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ import usuario from "./src/routes/userRoutes.js";
 import gato from "./src/routes/gatoRoutes.js";
 import login from "./src/routes/authRoutes.js";
 import administrador from "./src/routes/admRoutes.js";
+import authMiddleware from "./src/middleware/auth.middleware.js";
 
 const app = express();
 
-app.use(cors("*"), express.json(), usuario, gato, login, administrador);
+app.use(
+  cors("*"),
+  express.json(),
+  login,
+  authMiddleware,
+  usuario,
+  gato,
+  administrador
+);
 
 app.listen(3000, () => {
   console.log("Api rodando");
