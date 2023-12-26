970b62e7928b3e5a32b61982b91ee715771d49cb
JessycaTaynara <jessycatay16@gmail.com>
2023-12-26 08:28:16 -0300

    FEAT: js para div de alerta

--- src/view/UsuárioComum/home/index.js
+++ src/view/UsuárioComum/home/index.js
@@ -73,5 +73,29 @@ exibirGatos();
     localStorage.clear()
     window.location.href = '../login.html';
   }
+  function mostrarAlerta(mensagem, tipo){
+    const caixaDeAlerta = document.querySelector("div#caixaDeAlerta")
+    const texto = document.querySelector("strong#mensagemDeAlerta")
   
+    caixaDeAlerta.classList.remove("d-none")
+    caixaDeAlerta.classList.add("d-flex")
+  
+    caixaDeAlerta.classList.remove("alert-success")
+    caixaDeAlerta.classList.remove("alert-danger")
+    caixaDeAlerta.classList.remove("alert-warning")
+    
+    switch(tipo){
+      case "erro":
+        caixaDeAlerta.classList.add("alert-danger")
+        break
+      case "alerta":
+        caixaDeAlerta.classList.add("alert-warning")
+        break
+      case "sucesso":
+        caixaDeAlerta.classList.add("alert-success")
+        break
+    }
+  
+    texto.innerHTML = mensagem
+  }
   
