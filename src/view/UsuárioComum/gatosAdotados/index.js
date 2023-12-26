3a213bf633543842636b90de932d525636514ce5
JessycaTaynara <jessycatay16@gmail.com>
2023-12-26 10:54:32 -0300

    FEAT: js para mostrar alerta e ajustes na req dos gatos adotados

--- src/view/UsuárioComum/gatosAdotados/index.js
+++ src/view/UsuárioComum/gatosAdotados/index.js
@@ -1,32 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    carregarCard()
+document.addEventListener('DOMContentLoaded', async function () {
+    await carregarCard()
 });
 async function carregarCard(){
     const token = localStorage.getItem('token')
     const email = (JSON.parse(atob(token.split(".")[1]))).email
     try{
-        const resp = await fetch(`http://localhost:3000/meusGatosAdotados/${email}`)
+        const resp = await fetch(`http://localhost:3000/minhasAdocoes/${email}`, {
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }    
+        })
         const gatos = await resp.json()
 
-        const body = document.querySelector("body")
+        const main = document.querySelector("main")
         if(gatos.length > 0){
             gatos.forEach(gato => {
-                body.innerHTML+= `
-                <div class="card" style="width: 18rem;">
+                main.innerHTML+= `
+                <div class="card" style="width: 18rem; height: 300px">
                     <div class="card-body  d-flex flex-column">
                         <h5 class="card-title">${gato.nome}</h5>
                         <ul class="list-group">
                             <li class="list-group-item d-flex justify-content-between align-items-center">
-                            Idade
-                            <span class="badge bg-primary rounded-pill">${gato.idade}</span>
-                            </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center">
                             Sexo
-                            <span class="badge bg-primary rounded-pill">${gato.sexo}</span>
+                                <span class="badge bg-primary rounded-pill">${gato.sexo}</span>
                             </li>
                             <li class="list-group-item d-flex justify-content-between align-items-center">
                             Raça
-                            <span class="badge bg-primary rounded-pill">${gato.raca}</span>
+                                <span class="badge bg-primary rounded-pill">${gato.raca}</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                            Cor
+                                <span class="badge bg-primary rounded-pill">${gato.cor}</span>
                             </li>
                             <li class="list-group-item d-flex flex-column justify-content-between align-items-center">
                                 <strong>Descrição:</strong><br>
@@ -38,9 +43,34 @@ async function carregarCard(){
                 `
             });
         }else{
-            body.innerHTML = "<h1>Você não adotou nenhum gato</h1>"
+            main.innerHTML = "<h1>Você não adotou nenhum gato</h1>"
         }
     }catch(error){
-        alert(error.mensagem)
+        mostrarAlerta(error.mensagem, "erro")
     }
 }
+function mostrarAlerta(mensagem, tipo){
+    const caixaDeAlerta = document.querySelector("div#caixaDeAlerta")
+    const texto = document.querySelector("strong#mensagemDeAlerta")
+  
+    caixaDeAlerta.classList.remove("d-none")
+    caixaDeAlerta.classList.add("d-flex")
+  
+    caixaDeAlerta.classList.remove("alert-success")
+    caixaDeAlerta.classList.remove("alert-danger")
+    caixaDeAlerta.classList.remove("alert-warning")
+    
+    switch(tipo){
+      case "erro":
+        caixaDeAlerta.classList.add("alert-danger")
+        break
+      case "alerta":
+        caixaDeAlerta.classList.add("alert-warning")
+        break
+      case "sucesso":
+        caixaDeAlerta.classList.add("alert-success")
+        break
+    }
+  
+    texto.innerHTML = mensagem
+  }
