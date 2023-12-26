f19e2e1abfb9cb09b397c5ced1c3e8de5d3a55d5
JessycaTaynara <jessycatay16@gmail.com>
2023-12-26 10:28:30 -0300

    FIX: novo nome de colunas

--- src/models/adocoesModel.js
+++ src/models/adocoesModel.js
@@ -9,15 +9,15 @@ class AdocaoModel {
   }
   async getMinhasAdocoes(email) {
     const conn = await bancoDeDados.conectar();
-    const sql = "SELECT * FROM adocoes WHERE email_dono = ($1)";
+    const sql = "SELECT * FROM adocoes WHERE email_usuario = ($1)";
     const adocoes = await conn.query(sql, [email]);
     return adocoes.rows;
   }
   async fazerAdocao(idGato, emailDono) {
     const conn = await bancoDeDados.conectar();
-    const sql1 = "INSERT INTO adocoes (email_dono, id_gato) VALUES ($1, $2)";
+    const sql1 = "INSERT INTO adocoes (email_usuario, id_gato) VALUES ($1, $2)";
     const sql2 =
-      "UPDATE gatos_para_adotar   SET adotado = true WHERE id = ($1)";
+      "UPDATE gatos_para_adotar SET adotado = true WHERE id = ($1)";
     await conn.query(sql1, [emailDono, idGato]);
     await conn.query(sql2, [idGato]);
   }
