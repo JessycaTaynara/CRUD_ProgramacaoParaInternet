1c0d82c12ad8ca6ceb9267f31658c6854863795b
JessycaTaynara <jessycatay16@gmail.com>
2023-12-27 08:09:15 -0300

    FEAT: função para requisição da solicitação

--- src/view/UsuárioComum/solicitacao/index.js
+++ src/view/UsuárioComum/solicitacao/index.js
@@ -24,9 +24,42 @@ function mostrarAlerta(mensagem, tipo){
     texto.innerHTML = mensagem
 }
 async function solicitar(){
-    try {
-        
-    } catch (error) {
-        mostrarAlerta(error.message, "erro")
+    const nome = capitalizeWords(document.querySelector("input#nome").value)
+    const sexo = document.querySelector("select#sexo").value
+    const cor = capitalizeWords(document.querySelector("input#cor").value)
+    const descricao = document.querySelector("textarea#descricao").value
+    const raca = capitalizeWords(document.querySelector("input#raca").value)
+
+    if(!nome || !sexo || !cor || !descricao || !raca){
+        mostrarAlerta("Digite todos os campos de informação sobre o gato para fazer a solicitação", "alerta")
+    }else{
+        const gato = {nome, sexo, cor, descricao, raca}
+        const token = localStorage.getItem("token")
+        const emailSolicicitante = (JSON.parse(atob(decodeURIComponent(token.split(".")[1])))).email
+        try {
+            const retorno = await fetch("http://localhost:3000/fazerSolicitacao", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify({gato, emailSolicicitante})
+            })
+    
+            const mensagem = await retorno.json()
+    
+            if(retorno.ok){
+                mostrarAlerta(mensagem.message, "sucesso")
+            }else{
+                mostrarAlerta(mensagem.message, "erro")
+            }
+        } catch (error) {
+            mostrarAlerta(error.message, "erro")
+        }
     }
+}
+function capitalizeWords(str) {
+    return str.replace(/\b\w/g, function(char) {
+      return char.toUpperCase();
+    });
 }
