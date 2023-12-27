f5ec2057ce4b6e73cdfae45abefd27731ba458d4
JessycaTaynara <jessycatay16@gmail.com>
2023-12-27 11:04:10 -0300

    tentando fazer funcionar

--- src/middleware/checkrole.js
+++ src/middleware/checkrole.js
@@ -1,7 +1,6 @@
 const checkrole = (roles) => (req, res, next) => {
   const tipoDoUser = req.tipo;
-
-  if (!roles.includes(tipoDoUser)) {
+  if (!(roles.includes(tipoDoUser))) {
     return res
       .status(401)
       .send({ message: "Você não tem autorização para acessar essa página" });
