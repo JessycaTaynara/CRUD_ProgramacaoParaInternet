ce5615b2bc79dd636a26e6052c655634f8c3f906
sabiss <sabrinabezsilva@gmail.com>
2023-12-28 18:42:39 -0300

    FEAT: início - ver minhas solicitações

---
+++ src/view/UsuárioComum/minhasSolicitacoes/index.js
@@ -0,0 +1,33 @@
+function mostrarAlerta(mensagem, tipo){
+  const caixaDeAlerta = document.querySelector("div#caixaDeAlerta")
+  const texto = document.querySelector("strong#mensagemDeAlerta")
+
+  caixaDeAlerta.classList.remove("d-none")
+  caixaDeAlerta.classList.add("d-flex")
+
+  caixaDeAlerta.classList.remove("alert-success")
+  caixaDeAlerta.classList.remove("alert-danger")
+  caixaDeAlerta.classList.remove("alert-warning")
+  
+  switch(tipo){
+    case "erro":
+      caixaDeAlerta.classList.add("alert-danger")
+      break
+    case "alerta":
+      caixaDeAlerta.classList.add("alert-warning")
+      break
+    case "sucesso":
+      caixaDeAlerta.classList.add("alert-success")
+      break
+  }
+
+  texto.innerHTML = mensagem
+}
+function mostrarTelaDeLoading() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.display = 'flex';
+}
+function fecharTelaDeLoading() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.display = 'none';
+}
