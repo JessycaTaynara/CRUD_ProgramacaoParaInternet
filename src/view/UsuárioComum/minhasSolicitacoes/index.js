55ac07492f7d82e9ceedd4423ad9120d84588c11
JessycaTaynara <jessycatay16@gmail.com>
2023-12-28 09:36:21 -0300

    FEAT: arquivos iniciais da tela de VerMinhasSolicitacoes

---
+++ src/view/UsuárioComum/minhasSolicitacoes/index.js
@@ -0,0 +1,8 @@
+function mostrarTelaDeLoading() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.display = 'flex';
+  }
+function fecharTelaDeLoading() {
+    const loadingScreen = document.getElementById('loadingScreen');
+    loadingScreen.style.display = 'none';
+}
