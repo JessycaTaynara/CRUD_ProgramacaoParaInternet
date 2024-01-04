0341c0d72e6f5713fe1a07522d0ff835740f46f9
sabiss <gataespacial18@gmail.com>
2024-01-03 23:32:59 -0300

    FIX: nova string de conexão com o banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import pg from "pg";
 
 async function conectar() {
   const pool = new pg.Pool({
-    connectionString: `postgres://aluno_20201214010034:152042@177.136.201.182:5439/temp?schema=aluno_20201214010034`,
+    connectionString: `postgres://aluno_20201214010034:1520@177.136.200.206:5439/temp?schema=aluno_20201214010034`,
   });
   const conexaoBancoDeDados = await pool.connect();
   console.log("Banco de dados conectado!");
